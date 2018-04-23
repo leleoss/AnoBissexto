15e0b9d83f566654e89fc450f9f4d2e2c4885de3
user_acronus <user_acronus@senacsp.edu.br>
2018-04-23 13:43:14 -0300

    Anos bissexto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AnoBissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              int qtd=0;
+          for(int i=1910; i <=2018; i++){
+              if(i%4==0){
+                  Console.WriteLine(i + " - Este ano é bissexto");
+                  qtd++;
+              }
+          }
+          Console.WriteLine(qtd);
+        }
+    }
+}
